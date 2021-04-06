7f08592becb0846aa914a0d8a95ee6c334778f9c
Akash Pillai <akashp807@gmail.com>
2021-04-06 13:04:43 -0500

    Rotate log file every day

--- animetracker/__main__.py
+++ animetracker/__main__.py
@@ -1,16 +1,25 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler
 import os
 import time
 
 from dotenv import load_dotenv
 import schedule
 
-from .workflows import anilist_popularity
+from animetracker.workflows import anilist_popularity
 
 # Load environment variables and set logging
 load_dotenv()
-logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
-logging.info('Bot started')
+logname = 'logs/animetracker.log'
+os.makedirs(os.path.dirname('logs/'), exist_ok=True)
+handler = TimedRotatingFileHandler(logname, when='midnight', interval=1, encoding='utf8')
+handler.suffix = '%Y-%m-%d'
+formatter = logging.Formatter('%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+handler.setFormatter(formatter)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
+logger.info('Bot started')
 
 # Schedule bot routine
 schedule.every().day.at(os.environ['ANILIST_POPULARITY_START']).do(anilist_popularity)
